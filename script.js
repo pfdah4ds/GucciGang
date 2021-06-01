d7ba3a122b215a8d2c4469b738d8f70965af9c81
pfdah <pratikdahal700@gmail.com>
2021-06-01 13:42:43 +0545

    First commit upto basic dtypes

---
+++ script.js
@@ -0,0 +1,62 @@
+alert('Hello sexy people');
+
+//Variable Declaration
+console.log('Wtf kasari ayo name', name);
+var  name = 'Pratik'
+let names = 'Pratikd'
+console.log('Let garda chai', names);
+const PI = 3.14
+//changing shit
+name += 'Hehe';
+console.log(name);
+names += 'Hehe';
+console.log(names);
+//PI += 1;
+
+
+
+//Data Types
+    //String
+let fname = "Pratik";
+let lname = "D";
+let retu = '66';
+console.log(fname +' '+ lname+'\nage: ', retu);
+
+    //Number
+let age = 22;
+const goldenr = PI *2;
+console.log(age , ' + ' , goldenr ,' = ', age+goldenr );
+
+
+    //Boolean
+let truth = true;
+let falseth = false;
+console.log('Truth is ',truth,' and false is ',falseth);
+
+    //Undefined & Null
+let undef;
+let nulldt= null;
+console.log('undefinded be like:',undef);
+console.log('Null be like:',nulldt);
+
+
+//Symbol
+    //not using symbol
+let name1 ='Javascript'
+let name2 ='Javascript'
+console.log(name1 == name2);
+    //using symbol
+let name_1 = Symbol('Javascript');
+let name_2 = Symbol('Javascript');
+
+console.log(name_1 == name_2);
+console.log('Yo false auxa kina ki symbol le different banauxa eutai lai ni');
+
+//Checking datatype
+console.log(name_1.typeof());
+console.log(nulldt.typeof());
+
+
+
+
+
